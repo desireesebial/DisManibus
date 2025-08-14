7c108d6e28116454acf33577431d5b190838abea
Sam.Cuevas22 <samueljaztin@gmail.com>
2025-08-14 17:13:34 +0800

    DoorScript nagana na

---
+++ Assets/Scripts/doorscript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class doorscrpit : MonoBehaviour
+{
+    public float openAngle = 90f;
+    public float openSpeed = 2f;
+    public bool isOpen = false;
+
+    private Quaternion _closedRotation;
+    private Quaternion _openRotation;
+    private Coroutine _currentCoroutine;
+
+    void Start() // <-- must be capitalized so Unity calls it
+    {
+        _closedRotation = transform.rotation;
+        _openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_currentCoroutine != null) StopCoroutine(_currentCoroutine);
+            _currentCoroutine = StartCoroutine(ToggleDoor());
+        }
+    }
+
+    private IEnumerator ToggleDoor()
+    {
+        Quaternion targetRotation = isOpen ? _closedRotation : _openRotation;
+        Quaternion startRotation = transform.rotation;
+        isOpen = !isOpen;
+
+        float timeElapsed = 0f;
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        {
+            timeElapsed += Time.deltaTime * openSpeed;
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, timeElapsed);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation; // snap to final position
+    }
+}
