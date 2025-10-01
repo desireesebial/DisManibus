4e409f5cf546b280196865092ae204481bfa998f
Sam.Cuevas22 <samueljaztin@gmail.com>
2025-10-01 17:55:18 +0800

    Savestate function works

---
+++ Assets/Scripts/World/SceneTransition/CheckpointTrigger.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace World.SceneTransition
+{
+    [RequireComponent(typeof(Collider))]
+    public class CheckpointTrigger : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private SaveLoad.SaveManager saveManager;
+        [SerializeField] private string playerTag = "Player";
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent onCheckpointReached;
+
+        private bool hasSaved;
+
+        private void Reset()
+        {
+            var collider = GetComponent<Collider>();
+            collider.isTrigger = true;
+        }
+
+        private void Awake()
+        {
+            if (saveManager == null)
+            {
+                saveManager = FindAnyObjectByType<SaveLoad.SaveManager>();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(playerTag))
+            {
+                return;
+            }
+
+            if (hasSaved)
+            {
+                return;
+            }
+
+            if (saveManager == null)
+            {
+                Debug.LogWarning("CheckpointTrigger could not find SaveManager; no save performed.");
+                return;
+            }
+
+            saveManager.SaveGame();
+            hasSaved = true;
+            onCheckpointReached?.Invoke();
+            Debug.Log($"Checkpoint saved at '{name}'.");
+        }
+
+        public void ResetCheckpoint()
+        {
+            hasSaved = false;
+        }
+    }
+}
